c546d195821d39265b1307bd0533c2b8a6b046f8
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-07-16 20:24:26 +0530

    up

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const video = document.querySelector(".player");
 const canvas = document.querySelector(".photo");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
-
+const ctx = canvas.getContext('2d');
 
 function getVideo() {
   navigator.mediaDevices.getUserMedia( { video: true, audio: false } )
@@ -12,14 +12,31 @@ function getVideo() {
       video.srcObject = localMediaStream;
       video.play()
     })
+    .catch(() => {
+      console.log("Acces denied !!! ")
+    })
 }
 
+
+function paint() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+  canvas.width = width
+  canvas.height = height
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  },16)
+  
+}
 
 
 function takePhoto() {
-  console.log("hello")
+  snap.currentTime = 0
   snap.play()
 }
 
-
 getVideo()
+
+
+video.addEventListener("canplay", paint)
