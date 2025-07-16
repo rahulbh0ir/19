f6e51ee9a50467fc2c50d7fe1fa42a1df2b86235
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-07-16 09:34:35 +0530

    up

--- scripts.js
+++ scripts.js
@@ -1,5 +1,26 @@
 
 
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia( { video: true, audio: false } )
+    .then(localMediaStream => {
+      console.log(localMediaStream)
+      video.srcObject = localMediaStream;
+      video.play()
+    })
+}
+
+
+
 function takePhoto() {
   console.log("hello")
+  snap.play()
 }
+
+
+getVideo()
