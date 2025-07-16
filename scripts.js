82a58f45db49c58f142f87fb047a037a70a9bcd0
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-07-16 19:54:06 +0530

    up

--- scripts.js
+++ scripts.js
@@ -9,7 +9,6 @@ const snap = document.querySelector(".snap");
 function getVideo() {
   navigator.mediaDevices.getUserMedia( { video: true, audio: false } )
     .then(localMediaStream => {
-      console.log(localMediaStream)
       video.srcObject = localMediaStream;
       video.play()
     })
